289c310111fc6155052156ea339b5da53e67c465
Bartu Baş <bartubas99@gmail.com>
2024-04-11 14:39:37 +0300

    Extra SIMD Job

--- Assets/Code/Logic/Jobs/CullMultiJobSIMD.cs
+++ Assets/Code/Logic/Jobs/CullMultiJobSIMD.cs
@@ -12,7 +12,7 @@ namespace SphereCulling
 		public NativeArray<float3> Positions;
 
 		[DeallocateOnJobCompletion]
-		[ReadOnly] 
+		[ReadOnly]
 		public NativeArray<PlanePacket4> PlanePackets;
 
 		public NativeList<float4x4>.ParallelWriter Output;
@@ -22,7 +22,7 @@ namespace SphereCulling
 			var pos = Positions[index];
 			var p0 = PlanePackets[0];
 			var p1 = PlanePackets[1];
-            
+
 			// Inside: Radius + Dot(Normal, Point) + Distance > 0
 			bool4 masks = (p0.Xs * pos.x + p0.Ys * pos.y + p0.Zs * pos.z + p0.Distances + Constants.SphereRadius > 0) &
 			              (p1.Xs * pos.x + p1.Ys * pos.y + p1.Zs * pos.z + p1.Distances + Constants.SphereRadius > 0);
@@ -31,66 +31,6 @@ namespace SphereCulling
 			{
 				Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
 			}
-
-			// Shit, this requires batch job
-
-			// public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
-			// {
-			//     // Get arrays of the components in this chunk that we're interested in.
-			//     // Reinterpret the data as floats to make it easier to manipulate for packing
-			//     var chunkTransforms = chunk.GetNativeArray(ref LocalToWorldTypeHandle).AsReadOnly();
-			//     var chunkRadii = chunk.GetNativeArray(ref RadiusTypeHandle).Reinterpret<float>();
-			//     var chunkVis = chunk.GetNativeArray(ref VisibilityTypeHandle);
-			//    
-			//     var p0 = FP[0];
-			//     var p1 = FP[1];
-			//     var p2 = FP[2];
-			//     var p3 = FP[3];
-			//     var p4 = FP[4];
-			//     var p5 = FP[5];
-			//
-			//     for (var i = 0; chunk.Count - i >= 4; i += 4)
-			//     {
-			//         // Load 4 float3 positions, then "shuffle" them into vertical Xs, Ys and Zs 
-			//         var a = chunkTransforms[i].Position;
-			//         var b = chunkTransforms[i+1].Position;
-			//         var c = chunkTransforms[i+2].Position;
-			//         var d = chunkTransforms[i+3].Position;
-			//         var Xs = new float4(a.x, b.x, c.x, d.x);
-			//         var Ys = new float4(a.y, b.y, c.y, d.y);
-			//         var Zs = new float4(a.z, b.z, c.z, d.z);
-			//        
-			//         // Grab 4 radii in a single float4 
-			//         var Radii = chunkRadii.ReinterpretLoad<float4>(i);
-			//        
-			//         // Test each of the 6 planes against the 4 shuffled spheres
-			//         bool4 mask =
-			//             p0.x * Xs + p0.y * Ys + p0.z * Zs + p0.w + Radii > 0.0f &
-			//             p1.x * Xs + p1.y * Ys + p1.z * Zs + p1.w + Radii > 0.0f &
-			//             p2.x * Xs + p2.y * Ys + p2.z * Zs + p2.w + Radii > 0.0f &
-			//             p3.x * Xs + p3.y * Ys + p3.z * Zs + p3.w + Radii > 0.0f &
-			//             p4.x * Xs + p4.y * Ys + p4.z * Zs + p4.w + Radii > 0.0f &
-			//             p5.x * Xs + p5.y * Ys + p5.z * Zs + p5.w + Radii > 0.0f;
-			//
-			//         chunkVis.ReinterpretStore(i, new int4(mask));
-			//     }
-			//
-			//     // In case the number of entities in this chunk isn't neatly divisible by 4, cull the last few spheres individually
-			//     for (var i = (chunk.Count >> 2) << 2; i < chunk.Count; ++i)
-			//     {
-			//         var pos = chunkTransforms[i].Position;
-			//         var radius = chunkRadii[i];
-			//
-			//         int visible =
-			//             (math.dot(p0.xyz, pos) + p0.w + radius > 0.0f &&
-			//              math.dot(p1.xyz, pos) + p1.w + radius > 0.0f &&
-			//              math.dot(p2.xyz, pos) + p2.w + radius > 0.0f &&
-			//              math.dot(p3.xyz, pos) + p3.w + radius > 0.0f &&
-			//              math.dot(p4.xyz, pos) + p4.w + radius > 0.0f &&
-			//              math.dot(p5.xyz, pos) + p5.w + radius > 0.0f) ? 1 : 0;
-			//        
-			//         chunkVis[i] = new SphereVisible { Value = visible };
-			//     }
 		}
 	}
 }
---
+++ Assets/Code/Logic/Jobs/CullMultiJobSIMDExtra.cs
@@ -0,0 +1,82 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace SphereCulling
+{
+	[BurstCompile]
+	public struct CullMultiJobSIMDExtra : IJobParallelForBatch
+	{
+		[ReadOnly]
+		public NativeArray<float> Xs;
+
+		[ReadOnly]
+		public NativeArray<float> Ys;
+
+		[ReadOnly]
+		public NativeArray<float> Zs;
+
+		// [ReadOnly]
+		// public NativeArray<float> Radii;
+
+		[ReadOnly]
+		public NativeArray<float4> Planes;
+
+		public NativeList<float4x4>.ParallelWriter Output;
+
+		// TODO: Handle count not visible by 4 case
+		public void Execute(int startIndex, int count)
+		{
+			var p0 = Planes[0];
+			var p1 = Planes[1];
+			var p2 = Planes[2];
+			var p3 = Planes[3];
+			var p4 = Planes[4];
+			var p5 = Planes[5];
+
+			for (int i = 0; i < count; i += 4)
+			{
+				var idx = startIndex + i;
+				var xs = Xs.ReinterpretLoad<float4>(idx);
+				var ys = Ys.ReinterpretLoad<float4>(idx);
+				var zs = Zs.ReinterpretLoad<float4>(idx);
+				// var radii = Radii.ReinterpretLoad<float4>(idx);
+				const float radii = Constants.SphereRadius;
+
+				// Tests 4 Objects against 6 planes
+				bool4 mask = p0.x * xs + p0.y * ys + p0.z * zs + p0.w + radii > 0.0f &
+				             p1.x * xs + p1.y * ys + p1.z * zs + p1.w + radii > 0.0f &
+				             p2.x * xs + p2.y * ys + p2.z * zs + p2.w + radii > 0.0f &
+				             p3.x * xs + p3.y * ys + p3.z * zs + p3.w + radii > 0.0f &
+				             p4.x * xs + p4.y * ys + p4.z * zs + p4.w + radii > 0.0f &
+				             p5.x * xs + p5.y * ys + p5.z * zs + p5.w + radii > 0.0f;
+
+				// Normally you'd do reinterpretStore to visibility mask, but we're doing culling and adding at the same time.
+				if (mask.x)
+				{
+					var pos = new float3(xs[0], ys[0], zs[0]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (mask.y)
+				{
+					var pos = new float3(xs[1], ys[1], zs[1]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (mask.z)
+				{
+					var pos = new float3(xs[2], ys[2], zs[2]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (mask.w)
+				{
+					var pos = new float3(xs[3], ys[3], zs[3]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+			}
+		}
+	}
+}
