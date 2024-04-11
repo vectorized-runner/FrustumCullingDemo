1e20be1b777042f9300cc25448c88b2578bb11de
Bartu Baş <bartubas99@gmail.com>
2024-04-11 11:01:05 +0300

    OptimizeFor Performance

--- Assets/Code/SphereCullingManager.cs
+++ Assets/Code/SphereCullingManager.cs
@@ -72,7 +72,7 @@ public struct CullMultiJob : IJobParallelFor
 	}
 }
 
-[BurstCompile]
+[BurstCompile(OptimizeFor = OptimizeFor.Performance)]
 public struct CullMultiJobBurst : IJobParallelFor
 {
 	[ReadOnly]
@@ -93,7 +93,7 @@ public struct CullMultiJobBurst : IJobParallelFor
 	}
 }
 
-[BurstCompile]
+[BurstCompile(OptimizeFor = OptimizeFor.Performance)]
 public struct CullMultiJobBranchless : IJobParallelFor
 {
 	[ReadOnly]
@@ -123,6 +123,38 @@ public struct CullMultiJobBranchless : IJobParallelFor
 		}
 	}
 }
+
+[BurstCompile(OptimizeFor = OptimizeFor.Performance)]
+public struct CullMultiJobSIMD : IJobParallelFor
+{
+	[ReadOnly]
+	public NativeArray<float3> Positions;
+
+	[ReadOnly]
+	public NativeArray<DotsPlane> Planes;
+
+	public NativeList<float4x4>.ParallelWriter Output;
+
+	public void Execute(int index)
+	{
+		var position = Positions[index];
+
+		// Inside: R + N + D > 0
+		var result =
+			Planes[0].Distance + Constants.SphereRadius + math.dot(position, Planes[0].Normal) > 0 &&
+			Planes[1].Distance + Constants.SphereRadius + math.dot(position, Planes[1].Normal) > 0 &&
+			Planes[2].Distance + Constants.SphereRadius + math.dot(position, Planes[2].Normal) > 0 &&
+			Planes[3].Distance + Constants.SphereRadius + math.dot(position, Planes[3].Normal) > 0 &&
+			Planes[4].Distance + Constants.SphereRadius + math.dot(position, Planes[4].Normal) > 0 &&
+			Planes[5].Distance + Constants.SphereRadius + math.dot(position, Planes[5].Normal) > 0;
+
+		if (result)
+		{
+			Output.AddNoResize(float4x4.TRS(position, quaternion.identity, new float3(1, 1, 1)));
+		}
+	}
+}
+
 
 
 [Serializable]
