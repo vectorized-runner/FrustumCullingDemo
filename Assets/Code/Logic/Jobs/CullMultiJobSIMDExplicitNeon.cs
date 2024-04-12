3f3fc3b995c260b29486b574173773a5a79a8bb5
Bartu Baş <bartubas99@gmail.com>
2024-04-12 09:47:20 +0300

    This should be it

--- Assets/Code/Logic/Jobs/CullMultiJobSIMDExplicitNeon.cs
+++ Assets/Code/Logic/Jobs/CullMultiJobSIMDExplicitNeon.cs
@@ -1,5 +1,4 @@
 using Unity.Burst;
-using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
@@ -44,73 +43,72 @@ namespace SphereCulling
 			var xPtr = (float*)Xs.GetUnsafeReadOnlyPtr();
 			var yPtr = (float*)Ys.GetUnsafeReadOnlyPtr();
 			var zPtr = (float*)Zs.GetUnsafeReadOnlyPtr();
-			var results = stackalloc float[4];
+			var results = stackalloc uint[4];
 
 			for (int i = 0; i < count; i += 4)
 			{
 				var idx = startIndex + i;
-				
-				
-				
-				// v128: 128-bit, 32-byte, 4 floats
 				var xs = vld1q_f32(xPtr + idx);
 				var ys = vld1q_f32(yPtr + idx);
 				var zs = vld1q_f32(zPtr + idx);
 				var zero = vdupq_n_f32(0.0f);
 				var radii = vdupq_n_f32(Constants.SphereRadius);
-				var res0 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p0.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p0.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p0.z), zs)), X86.Sse.set_ps1(p0.w)), radii), zero);
-				var res1 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p1.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p1.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p1.z), zs)), X86.Sse.set_ps1(p1.w)), radii), zero);
-				var res2 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p2.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p2.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p2.z), zs)), X86.Sse.set_ps1(p2.w)), radii), zero);
-				var res3 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p3.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p3.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p3.z), zs)), X86.Sse.set_ps1(p3.w)), radii), zero);
-				var res4 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p4.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p4.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p4.z), zs)), X86.Sse.set_ps1(p4.w)), radii), zero);
-				var res5 = X86.Sse.cmpgt_ps(
-					X86.Sse.add_ps(
-						X86.Sse.add_ps(
-							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p5.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p5.y), ys)),
-								X86.Sse.mul_ps(X86.Sse.set_ps1(p5.z), zs)), X86.Sse.set_ps1(p5.w)), radii), zero);
-				var res = X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(res0, res1), res2), res3), res4), res5);
+				var res0 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p0.x), xs), vmulq_f32(vdupq_n_f32(p0.y), ys)),
+								vmulq_f32(vdupq_n_f32(p0.z), zs)), vdupq_n_f32(p0.w)), radii), zero);
+				var res1 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p1.x), xs), vmulq_f32(vdupq_n_f32(p1.y), ys)),
+								vmulq_f32(vdupq_n_f32(p1.z), zs)), vdupq_n_f32(p1.w)), radii), zero);
+				var res2 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p2.x), xs), vmulq_f32(vdupq_n_f32(p2.y), ys)),
+								vmulq_f32(vdupq_n_f32(p2.z), zs)), vdupq_n_f32(p2.w)), radii), zero);
+				var res3 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p3.x), xs), vmulq_f32(vdupq_n_f32(p3.y), ys)),
+								vmulq_f32(vdupq_n_f32(p3.z), zs)), vdupq_n_f32(p3.w)), radii), zero);
+				var res4 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p4.x), xs), vmulq_f32(vdupq_n_f32(p4.y), ys)),
+								vmulq_f32(vdupq_n_f32(p4.z), zs)), vdupq_n_f32(p4.w)), radii), zero);
+				var res5 = vcgtq_f32(
+					vaddq_f32(
+						vaddq_f32(
+							vaddq_f32(vaddq_f32(vmulq_f32(vdupq_n_f32(p5.x), xs), vmulq_f32(vdupq_n_f32(p5.y), ys)),
+								vmulq_f32(vdupq_n_f32(p5.z), zs)), vdupq_n_f32(p5.w)), radii), zero);
 
-				X86.Sse.storeu_ps(results, res);
+				// vcgtq_f32: works on float32x4, returns 128-bit vector mask, 0xFFFFFFFF or 0x00000000 for each 4 values.
+				// Use integer representation to apply bitwise
+				var res = vandq_u32(vandq_u32(vandq_u32(vandq_u32(vandq_u32(res0, res1), res2), res3), res4), res5);
 
-				if (results[0] > 0f)
+				vst1q_u32(results, res);
+
+				if (results[0] != 0)
 				{
 					var pos = new float3(xPtr[idx + 0], yPtr[idx + 0], zPtr[idx + 0]);
 					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
 				}
 
-				if (results[1] > 0f)
+				if (results[1] != 0)
 				{
 					var pos = new float3(xPtr[idx + 1], yPtr[idx + 1], zPtr[idx + 1]);
 					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
 				}
 
-				if (results[2] > 0f)
+				if (results[2] != 0)
 				{
 					var pos = new float3(xPtr[idx + 2], yPtr[idx + 2], zPtr[idx + 2]);
 					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
 				}
 
-				if (results[3] > 0f)
+				if (results[3] != 0)
 				{
 					var pos = new float3(xPtr[idx + 3], yPtr[idx + 3], zPtr[idx + 3]);
 					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
