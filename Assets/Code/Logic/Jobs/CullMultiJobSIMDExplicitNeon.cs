e00e3ff296be06b6bd5d9565478d45398c92fbec
Bartu Baş <bartubas99@gmail.com>
2024-04-12 09:21:58 +0300

    WIP

---
+++ Assets/Code/Logic/Jobs/CullMultiJobSIMDExplicitNeon.cs
@@ -0,0 +1,121 @@
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using static Unity.Burst.Intrinsics.Arm.Neon;
+
+namespace SphereCulling
+{
+	[BurstCompile]
+	public unsafe struct CullMultiJobSIMDExplicitNeon : IJobParallelForBatch
+	{
+		[ReadOnly]
+		public NativeArray<float> Xs;
+
+		[ReadOnly]
+		public NativeArray<float> Ys;
+
+		[ReadOnly]
+		public NativeArray<float> Zs;
+
+		// [ReadOnly]
+		// public NativeArray<float> Radii;
+
+		[ReadOnly]
+		public NativeArray<float4> Planes;
+
+		public NativeList<float4x4>.ParallelWriter Output;
+
+		public void Execute(int startIndex, int count)
+		{
+			// Count not divisible by 4 not handled here
+			Debug.Assert(count % 4 == 0);
+			Debug.Assert(IsNeonSupported);
+
+			var p0 = Planes[0];
+			var p1 = Planes[1];
+			var p2 = Planes[2];
+			var p3 = Planes[3];
+			var p4 = Planes[4];
+			var p5 = Planes[5];
+			var xPtr = (float*)Xs.GetUnsafeReadOnlyPtr();
+			var yPtr = (float*)Ys.GetUnsafeReadOnlyPtr();
+			var zPtr = (float*)Zs.GetUnsafeReadOnlyPtr();
+			var results = stackalloc float[4];
+
+			for (int i = 0; i < count; i += 4)
+			{
+				var idx = startIndex + i;
+				
+				
+				
+				// v128: 128-bit, 32-byte, 4 floats
+				var xs = vld1q_f32(xPtr + idx);
+				var ys = vld1q_f32(yPtr + idx);
+				var zs = vld1q_f32(zPtr + idx);
+				var zero = vdupq_n_f32(0.0f);
+				var radii = vdupq_n_f32(Constants.SphereRadius);
+				var res0 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p0.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p0.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p0.z), zs)), X86.Sse.set_ps1(p0.w)), radii), zero);
+				var res1 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p1.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p1.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p1.z), zs)), X86.Sse.set_ps1(p1.w)), radii), zero);
+				var res2 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p2.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p2.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p2.z), zs)), X86.Sse.set_ps1(p2.w)), radii), zero);
+				var res3 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p3.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p3.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p3.z), zs)), X86.Sse.set_ps1(p3.w)), radii), zero);
+				var res4 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p4.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p4.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p4.z), zs)), X86.Sse.set_ps1(p4.w)), radii), zero);
+				var res5 = X86.Sse.cmpgt_ps(
+					X86.Sse.add_ps(
+						X86.Sse.add_ps(
+							X86.Sse.add_ps(X86.Sse.add_ps(X86.Sse.mul_ps(X86.Sse.set_ps1(p5.x), xs), X86.Sse.mul_ps(X86.Sse.set_ps1(p5.y), ys)),
+								X86.Sse.mul_ps(X86.Sse.set_ps1(p5.z), zs)), X86.Sse.set_ps1(p5.w)), radii), zero);
+				var res = X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(X86.Sse.and_ps(res0, res1), res2), res3), res4), res5);
+
+				X86.Sse.storeu_ps(results, res);
+
+				if (results[0] > 0f)
+				{
+					var pos = new float3(xPtr[idx + 0], yPtr[idx + 0], zPtr[idx + 0]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (results[1] > 0f)
+				{
+					var pos = new float3(xPtr[idx + 1], yPtr[idx + 1], zPtr[idx + 1]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (results[2] > 0f)
+				{
+					var pos = new float3(xPtr[idx + 2], yPtr[idx + 2], zPtr[idx + 2]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+
+				if (results[3] > 0f)
+				{
+					var pos = new float3(xPtr[idx + 3], yPtr[idx + 3], zPtr[idx + 3]);
+					Output.AddNoResize(float4x4.TRS(pos, quaternion.identity, new float3(1, 1, 1)));
+				}
+			}
+		}
+	}
+}
